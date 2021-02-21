0bff15d4c5e246deb09bcc02248f13ee624431ce
BringbangJb <jane_spr@hotmail.com>
2021-02-21 11:47:13 +0700

    version2

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -13,7 +13,15 @@ class ProfileController extends Controller
             "full_name" => "Nitit Duangsanglek",
             "email"     => "jane_spr@hotmail.com",
             "phone"     => "-",
-            "mobile"    => "086-XXXXXXX"
+            "mobile"    => "086-XXXXXXX",
+            "address"  => "45/45 m.6 kukot lumluka pathumthani 12130",
+            "website" => "https://bringbang.medium.com/",
+            "github" => "bringbang",
+            "twitter" => "bringbang",
+            "Instagram" => "bringbang_",
+            "facebook" => "Nitit Duangsanglek",
+            "education" => "Silpakorn University",
+
         ]);
     }
 }
